4cacc2015bc98f683b973f295ea537963ee8fad0
abhilash215 <abhiu215@gmail.com>
2017-04-20 15:24:31 -0400

    Mapper and reducer added

--- src/com/AirTravel/AirTravelMapper.java
+++ src/com/AirTravel/AirTravelMapper.java
@@ -3,5 +3,27 @@ package com.AirTravel;
 /**
  * Created by abhilashuday on 4/20/17.
  */
-public class AirTravelMapper {
+import java.io.IOException;
+// import org.apache.commons.logging.Log;
+// import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import org.apache.hadoop.io.Text;
+
+import javax.naming.Context;
+
+
+/**
+ *
+ * @author mac
+ */
+public class AirTravelMapper extends Mapper<Text,Text,Text,Text> {
+    // The Karmasphere Studio Workflow Log displays logging from Apache Commons Logging, for example:
+    // private static final Log LOG = LogFactory.getLog("org.smaple.HomeworkMapper");
+
+    @Override
+    protected void map(Text key, Text value, Context context)
+            throws IOException, InterruptedException {
+        // TODO: please implement your mapper code here
+    }
 }
--- src/com/AirTravel/AirTravelReducer.java
+++ src/com/AirTravel/AirTravelReducer.java
@@ -3,5 +3,26 @@ package com.AirTravel;
 /**
  * Created by abhilashuday on 4/20/17.
  */
-public class AirTravelReducer {
+
+
+
+        import java.io.IOException;
+// import org.apache.commons.logging.Log;
+// import org.apache.commons.logging.LogFactory;
+        import org.apache.hadoop.mapreduce.Reducer;
+        import org.apache.hadoop.io.Text;
+
+/**
+ *
+ * @author mac
+ */
+public class AirTravelReducer extends Reducer<Text,Text,Text,Text> {
+    // The Karmasphere Studio Workflow Log displays logging from Apache Commons Logging, for example:
+    // private static final Log LOG = LogFactory.getLog("org.smaple.HomeworkReducer");
+
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context)
+            throws IOException, InterruptedException {
+        // TODO: please implement your reducer here
+    }
 }

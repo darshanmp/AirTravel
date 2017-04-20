733792eef426240a4f7796e0bde7e3b60d454ef2
abhilash215 <abhiu215@gmail.com>
2017-04-20 13:54:46 -0400

    Initial commit

---
+++ src/com/AirTravel/AirTravelJob.java
@@ -0,0 +1,10 @@
+package com.AirTravel;
+
+public class AirTravelJob {
+
+    public static void main(String[] args) {
+
+        System.out.println(" initial file and commit ");
+	// write your code here
+    }
+}

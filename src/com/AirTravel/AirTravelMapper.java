9784f7327e8caf1a36c08576250b7ffd48d6dd85
Darshan Masti Prakash <dmastipr@syr.edu>
2017-04-25 19:31:20 -0400

    committing mapper files

--- src/com/AirTravel/AirTravelMapper.java
+++ src/com/AirTravel/AirTravelMapper.java
@@ -24,7 +24,7 @@ public class AirTravelMapper extends Mapper<Text,Text,Text,Text> {
     @Override
     protected void map(Text key, Text value, Context context)
             throws IOException, InterruptedException {
-    	//darshan first comment
+    	// Darshan First comment
         // TODO: please implement your mapper code here
     }
 }
